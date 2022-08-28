1b86e92ebab59a9ccdd7c888f181ab5bc9aca6ee
Mehmet Enes KAPLAN <m.enes.kaplan0@gmail.com>
2022-08-28 14:07:50 +0300

    Larry's Array

---
+++ HackerRank/2-medium/python/larrysArray.py
@@ -0,0 +1,10 @@
+def larrysArray(A):
+    counter=0
+    for i in range(len(A)):
+        for j in range(i+1,len(A)):
+            if A[i]>A[j]:
+                counter+=1
+    if counter%2==0:
+        return "YES"
+    return "NO"
+
